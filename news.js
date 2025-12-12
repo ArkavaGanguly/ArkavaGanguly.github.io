9eff5f27e2def4696b9584b7236a546da9d8c3b6
ArkavaGanguly <90079736+ArkavaGanguly@users.noreply.github.com>
2025-12-11 20:28:21 -0700

    Add news items for December and November 2025

---
+++ news.js
@@ -0,0 +1,15 @@
+// news.js
+window.NEWS_ITEMS = [
+  {
+    date: "2025-12-08",
+    label: "Dec 2025",
+    text: "Successfully defended my PhD thesis!",
+    link: null
+  },
+  {
+    date: "2025-11-06",
+    label: "Nov 2025",
+    text: "Attended AIChE Annual Meeting at Boston",
+    link: null
+  }
+];
